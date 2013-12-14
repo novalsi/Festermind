7a76cef3826b87843db2e083c2678bafc7788d71
Nick Smith <novemberalphasierra@gmail.com>
2013-12-13 20:52:27 -0500

    First Commit
    NIB fully set up with IBOutlets for each peg, hole, and dot

---
+++ Festermind/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Festermind
+//
+//  Created by Student on 12/13/13.
+//  Copyright (c) 2013 Nick Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Festermind/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Festermind
+//
+//  Created by Student on 12/13/13.
+//  Copyright (c) 2013 Nick Smith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
